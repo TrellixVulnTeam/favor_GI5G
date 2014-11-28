afe951077a27f1c2c480cdb72dd53d5d92d2f458
Josh Tanner <mindful.jt@gmail.com>
2014-11-28 14:09:38 +0900

    tentatively finalize cache structure

--- src/processor.cpp
+++ src/processor.cpp
@@ -16,15 +16,39 @@ namespace favor {
                         (std::hash<long>()(t) << 2));
             }
 
-            class Result {
+            class Result;
+
+            class ResultKey {
             private:
                 const ResultType type;
-                long fromDate;
-                long untilDate;
-                long contactId;
+                const long fromDate;
+                const long untilDate;
+                vector<long> contacts;
+                vector<std::pair<string, MessageType>> accounts;
+
+            public:
+                friend class ResultKeyHasher;
+
+                ResultKey(ResultType t, const vector<AccountManager*>& accs, const vector<Contact>& cons, long fromD, long untilD) : type(t), fromDate(fromD), untilDate(untilD) {
+                    for (auto it = accs.begin(); it != accs.end(); ++it) accounts.push_back(std::pair<string, MessageType>((*it)->accountName, (*it)->type));
+                    for (auto it = cons.begin(); it != cons.end(); ++it) contacts.push_back(it->id);
+                }
+
+                bool operator==(const ResultKey& other) const{
+                    return (type == other.type &&
+                            fromDate == other.fromDate &&
+                            untilDate == other.untilDate &&
+                            contacts == other.contacts &&
+                            accounts == other.accounts);
+                }
+
+            };
+
+            class Result {
+            private:
                 const shared_ptr<void> data; //http://stackoverflow.com/questions/5913396/why-do-stdshared-ptrvoid-work - wizardy.
                 // TODO: verification of wizardy, specifically just write a tiny test with similar assignment and make sure destructors are firing properly
-                Result(ResultType t, long contact, long fromD, long untilD) : type(t), fromDate(fromD), untilDate(untilD), contactId(contact) {}
+                Result() {}
 
                 template <typename T>
                 void setValue(T input){
@@ -32,8 +56,8 @@ namespace favor {
                 }
 
             public:
-                template <typename T>
-                friend Result makeResult(ResultType t, long contact, long fromD, long untilD, T input);
+                template<typename T>
+                Result makeResult(T input);
 
                 template<typename T>
                 T getData(){
@@ -41,47 +65,67 @@ namespace favor {
                     return *ptr;
                 }
 
-                size_t key(){
-                    return resultKey(type, fromDate, untilDate, contactId);
-                }
             };
 
             template<typename T>
-            Result makeResult(ResultType t, long contact, long fromD, long untilD, T input){
-                Result ret(t, contact, fromD, untilD);
+            Result makeResult(T input){
+                Result ret;
                 ret.setValue<T>(input);
                 return ret;
             }
 
+            ResultKey makeResultKey(ResultType t, const vector<AccountManager*>* const accounts, const vector<Contact>* const contacts, long fromD, long untilD) {
+                const vector<AccountManager*>& accountsInput = accounts != NULL ? *accounts : vector<AccountManager*>();
+                const vector<Contact>& contactsInput = contacts != NULL ? *contacts : vector<Contact>();
 
-            std::unordered_map<size_t, Result> cache;
-            std::unordered_map<size_t, std::mutex> cacheLocks;
+                return ResultKey(t, accountsInput, contactsInput, fromD, untilD);
+            }
+
+            ResultKey makeResultKey(ResultType t, AccountManager* account, const Contact* const contact, long fromD, long untilD){
+                vector<AccountManager*> accountsInput;
+                vector<Contact> contactsInput;
+                if (account != NULL) accountsInput.push_back(account);
+                if (contact != NULL) contactsInput.push_back(*contact);
+
+                return ResultKey(t, accountsInput, contactsInput, fromD, untilD);
+            }
+
+            struct ResultKeyHasher{
+                std::size_t operator()(const ResultKey& k) const{
+                    size_t ret = std::hash<long>()(k.fromDate) ^ std::hash<long>()(k.untilDate);
+                    for (auto it = k.contacts.begin(); it != k.contacts.end(); ++it) ret ^ std::hash<long>()(*it);
+                    for (auto it = k.accounts.begin(); it != k.accounts.end(); ++it) ret ^ std::hash<string>()(it->first);
+                }
+            };
+
+
+            std::unordered_map<ResultKey, Result, ResultKeyHasher> cache;
+            std::mutex cacheLock; //The possibility of rehashes means we have to lock around the whole datastructure
 
 
             template<typename T>
-            void cacheResult(ResultType t, long contactId, long fromDate, long untilDate, T& input){
-                Result res = makeResult(t, contactId, fromDate, untilDate, input);
-                cacheLocks[res.key()].lock();
-                cache[res.key()] = res;
-                cacheLocks[res.key()].unlock();
+            void cacheResult(ResultType t, AccountManager* account, const Contact* const contact, long fromD, long untilD, T input){
+                ResultKey key = makeResultKey(t, account, contact, fromD, untilD);
+                cacheLock.lock();
+                cache[key] = makeResult<T>(input);
+                cacheLock.unlock();
             }
 
             template<typename T>
-            T getResult(ResultType t, long contactId, long fromDate, long untilDate){
-                long key = resultKey(t, contactId, fromDate, untilDate);
-                cacheLocks[key].lock();
-                T data = cache.at(key).getData<T>();
-                cacheLocks[key].unlock();
-                return data;
+            T getResult(ResultType t, AccountManager* account, const Contact* const contact, long fromD, long untilD){
+                ResultKey key = makeResultKey(t, account, contact, fromD, untilD);
+                cacheLock.lock();
+                T ret = cache.at(key);
+                cacheLock.unlock();
+                return ret;
             }
 
-            bool countResult(ResultType t, long contactId, long fromDate, long untilDate){
-                long key = resultKey(t, contactId, fromDate, untilDate);
-                cacheLocks[key].lock();
-                bool result = cache.count(resultKey(t, contactId, fromDate, untilDate));
-                cacheLocks[key].unlock();
-                return result;
-
+            bool countResult(ResultType t, AccountManager* account, const Contact* const contact, long fromD, long untilD){
+                ResultKey key = makeResultKey(t, account, contact, fromD, untilD);
+                cacheLock.lock();
+                bool count = cache.count(key);
+                cacheLock.unlock();
+                return count;
             }
         }
 
