03741828ba8fec7fde8f0845dea13a5334dd1ab2
Josh Tanner <mindful.jt@gmail.com>
2014-12-02 23:28:54 +0900

    python-cation writing data gen code
    fix

--- test/data_generator.py
+++ test/data_generator.py
@@ -9,33 +9,110 @@ TYPE_EMAIL = 0
 TYPE_LINE = 2
 
 
-class Address:
-    def __init__(self, name, addrType):
-        self.name = name
-        self.addrType = addrType
-
-
-class Account:
-    def __init__(self, name, accountType):
-        self.name = name
-        self.accountType = accountType
-        self.metrics = {}
-    #     self.addresses = []
-    #
-    # def add_account(self, account):
-    #     self.accounts
-
-
-ADDRESSES = [Address("test1@test.com", TYPE_EMAIL), Address("test2@test.com", TYPE_EMAIL),
-             Address("test3@test.com", TYPE_EMAIL),
-             Address("test4@test.com", TYPE_EMAIL), Address("Test1", TYPE_LINE), Address("Test2", TYPE_LINE)]
-
-ACCOUNTS = [Account("account1@test.com", TYPE_EMAIL), Account("account2@test.com", TYPE_EMAIL),
-            Account("account3", TYPE_LINE)]
-
 TYPENAMES = {TYPE_EMAIL: "email", TYPE_LINE: "line"}
 
-ID = 0
+
+def list_to_string(list):
+    return ",".join(str(x) for x in list)
+
+
+
+class Track:
+    IDS = {}
+    MAPS = {}
+
+
+    def __init__(self, name, cls):
+        self.name = name
+        if cls not in Track.IDS:
+            Track.IDS[cls] = 0
+
+        if cls not in Track.MAPS:
+            Track.MAPS[cls] = {}
+
+        if name in Track.MAPS[cls]:
+            self.id = Track.MAPS[cls][name].id
+        else:
+            Track.IDS[cls] += 1
+            self.id = Track.IDS[cls]
+            Track.MAPS[cls][name] = self
+
+class Address(Track):
+
+    def __init__(self, name, addrType, count):
+        super().__init__(name, self.__class__)
+        self.addrType = addrType
+        self.count = count
+        self.contact = None
+
+    def sql(self):
+        if self.contact:
+            contact_id = self.contact.id
+        else:
+            contact_id = "NULL"
+        return "INSERT INTO addresses_"+TYPENAMES[self.addrType]+" VALUES ("+list_to_string(['"'+self.name+'"',
+                                                                                            self.count,
+                                                                                            contact_id])+");"
+
+
+class Contact(Track):
+
+    def __init__(self, name, count):
+        super().__init__(name, self.__class__)
+        self.addresses = []
+
+
+    def add_address(self, account):
+        account.contact = self
+        self.addresses.append(account)
+
+
+    def sql(self):
+        #This is only a little complicated because we have to translate types into keys
+        active_types = {}
+        for address in self.addresses:
+            active_types[address.addrType] = True
+
+        flag = 0
+        for key in active_types:
+            flag += 2**key
+        return "INSERT INTO contacts VALUES("+list_to_string([self.id, '"'+self.name+'"', flag])+");"
+
+
+class Account(Track):
+
+    def __init__(self, name, accountType):
+        super().__init__(name, self.__class__)
+        self.accountType = accountType
+        self.metrics = {}
+
+    def sql(self):
+        return "TODO :("
+
+MSG_ID = 0
+
+def init():
+    con1 = Contact("EmailTest1", 1) #Email
+    con1.add_address(Address("test3@test.com", TYPE_EMAIL, 1))
+
+    con2 = Contact("LineTest2", 4) #Line
+    con2.add_address(Address("Test1", TYPE_LINE, 2))
+
+    con3 = Contact("LineEmailTest3", 5) #Line + Email
+    con3.add_address(Address("Test2", TYPE_LINE, 1))
+    con3.add_address(Address("test4@test.com", TYPE_EMAIL, 2))
+
+    #So they live in the class list
+    Address("test1@test.com", TYPE_EMAIL, 1)
+    #Address("test2@test.com", TYPE_EMAIL, 2) #Skipping this right now to slim down the data a bit
+
+    #Contact("EmailTest4", 1) #Contacts without addresses are basically meaningless, skipping these for now
+    #Contact("EmailTest5", 1)
+
+    Account("account1@test.com", TYPE_EMAIL)
+    Account("account2@test.com", TYPE_EMAIL)
+    Account("account3", TYPE_LINE)
+
 
 
 def rint(max):
@@ -44,7 +121,7 @@ def rint(max):
 
 #Account string, address object
 def generate_row(account, addr):
-    global ID
+    global MSG_ID
 
     if addr.name not in account.metrics:
         account.metrics[addr.name] = {}
@@ -74,9 +151,9 @@ def generate_row(account, addr):
         account.metrics[addr.name]["msgcount_received"] += 1
 
     sql += '" VALUES(' + ",".join(
-        str(x) for x in [ID, '"'+address.name+'"', int(msg_date.timestamp()), msg_charcount, getrandbits(1),
+        str(x) for x in [MSG_ID, '"'+address.name+'"', int(msg_date.timestamp()), msg_charcount, getrandbits(1),
                          '"Test message body"']) + ");"
-    ID += 1
+    MSG_ID += 1
     return sql
 
 
@@ -88,16 +165,22 @@ def format_defstring(string):
 
 
 if __name__ == '__main__':
+    init()
+    block_end = "\"\"\n\n"
+
+    #Write the message data
     out = open('testdata.h', 'w')
     out.write('#define MESSAGE_TEST_DATA ')
-    for account in ACCOUNTS:
-        for address in ADDRESSES:
+    for account in Track.MAPS[Account].values():
+        for address in Track.MAPS[Address].values():
             if address.addrType == account.accountType:
                 for i in range(40):
                     out.write(format_row(generate_row(account, address)))
 
-    out.write("\"\"\n\n")
-    for account in ACCOUNTS:
+    out.write(block_end)
+
+    #Write the summation data about messages
+    for account in Track.MAPS[Account].values():
         out.write('//'+account.name + ":" + str(account.metrics)+"\n")
         for address in account.metrics:
             for metric in account.metrics[address]:
@@ -106,3 +189,25 @@ if __name__ == '__main__':
                 out.write("#define "+definition+" "+str(account.metrics[address][metric])+"\n")
 
         out.write("\n")
+
+    #Write out the data to generate addresses
+    out.write('#define ADDRESS_TEST_DATA ')
+    for address in Track.MAPS[Address].values():
+        out.write(format_row(address.sql()))
+
+    out.write(block_end)
+
+    #Write out the data to generate contacts
+    out.write('#define CONTACT_TEST_DATA ')
+    for contact in Track.MAPS[Contact].values():
+        out.write(format_row(contact.sql()))
+
+    out.write(block_end)
+
+    #Write out the data to generate accounts
+    out.write('#define ACCOUNT_TEST_DATA ')
+    for account in Track.MAPS[Account].values():
+        out.write(format_row(account.sql()))
+
+
+    out.close()
