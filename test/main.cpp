8f1edd69cb4f0a9f7a3e4eb9c9363b7a7fc10239
Josh Tanner <mindful.jt@gmail.com>
2016-01-24 14:56:05 -0800

    skypemanager test work

--- test/main.cpp
+++ test/main.cpp
@@ -5,7 +5,7 @@
 #include "accountmanager_tests.h"
 #include "processor_tests.h"
 #include "managers/emailmanager_tests.h"
-#include "skypemanager_tests.h"
+#include "managers/skypemanager_tests.h"
 
 
 #include "gtest/gtest.h"
